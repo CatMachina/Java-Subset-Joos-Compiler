1396403d65428b1ff8ae2bcb8c69bd7fe3ad0756
e32xu <e32xu@uwaterloo.ca>
2025-01-26 19:01:18 -0500

    redesign ast to support parse tree after lexer

--- src/ast/astNode.hpp
+++ src/ast/astNode.hpp
@@ -7,17 +7,12 @@
 
 namespace ast {
 
-// Utility function for indentation
-inline std::string indent(int indentation) {
-    return std::string(indentation * 2, ' ');
-}
-
 // Base class for all AST nodes //////////////////////////////////////////////
 
 class AstNode {
 public:
     virtual ~AstNode() = default;
-    virtual std::ostream& print(std::ostream& os, int indentation = 0) const = 0;
+    virtual std::ostream& print(std::ostream& os) const = 0;
 };
 
 class Decl : public AstNode {
@@ -26,29 +21,23 @@ class Decl : public AstNode {
 public:
     explicit Decl(std::string_view name) : name{name} {}
     [[nodiscard]] std::string getName() const noexcept { return name; }
-
-    std::ostream& print(std::ostream& os, int indentation = 0) const override {
-        return os << indent(indentation) << "Decl: " << name;
-    }
 };
 
-class DeclContext : public AstNode {};
+class ProgramDecl : public AstNode {};
 
 class Type : public AstNode {
 public:
     ~Type() override = default;
     [[nodiscard]] virtual std::string toString() const = 0;
 
-    std::ostream& print(std::ostream& os, int indentation = 0) const override {
-        return os << indent(indentation) << toString();
+    std::ostream& print(std::ostream& os) const override {
+        return os << toString();
     }
 };
 
 class Stmt : public AstNode {};
 
-inline std::ostream& operator<<(std::ostream& os, const AstNode& astNode) {
-    return astNode.print(os);
-}
+std::ostream& operator<<(std::ostream& os, const AstNode& astNode);
 
 // Other classes /////////////////////////////////////////////////////////////
 
@@ -61,8 +50,8 @@ class Modifiers {
     bool isNative_ = false;
 
 public:
-    void set(parsetree::Modifier modifier); // Implementation needed
-    void set(ast::Modifiers modifier);      // Implementation needed
+    void set(parsetree::Modifier modifier);
+    void set(ast::Modifiers modifier);
 
     [[nodiscard]] bool isPublic() const noexcept { return isPublic_; }
     [[nodiscard]] bool isProtected() const noexcept { return isProtected_; }
@@ -71,12 +60,12 @@ public:
     [[nodiscard]] bool isAbstract() const noexcept { return isAbstract_; }
     [[nodiscard]] bool isNative() const noexcept { return isNative_; }
 
-    void setPublic() noexcept { isPublic_ = true; }
-    void setProtected() noexcept { isProtected_ = true; }
-    void setStatic() noexcept { isStatic_ = true; }
-    void setFinal() noexcept { isFinal_ = true; }
-    void setAbstract() noexcept { isAbstract_ = true; }
-    void setNative() noexcept { isNative_ = true; }
+    void setPublic();
+    void setProtected();
+    void setStatic();
+    void setFinal();
+    void setAbstract();
+    void setNative();
 
     [[nodiscard]] std::string toString() const;
 };
@@ -96,7 +85,7 @@ public:
         for (const auto& identifier : identifiers) {
             result += identifier + '.';
         }
-        result.pop_back(); // Remove trailing dot
+        result.pop_back();
         return result;
     }
 
--- src/ast/parseTree/parseTree.hpp
+++ src/ast/parseTree/parseTree.hpp
@@ -2,7 +2,6 @@
 
 #include <array>
 #include <iostream>
-#include <memory_resource>
 #include <string>
 #include <type_traits>
 #include <string_view>
@@ -26,15 +25,12 @@ class Operator;
 class Modifier;
 class BasicType;
 
-// For efficiently allocate memory for the nodes in the parse tree
-using BumpPtrAllocator = std::pmr::polymorphic_allocator<std::byte>;
-
-/// @brief The basic type-tagged node in the parse tree.
+// The base node in the parse tree.
 struct Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
-   /// @brief Enum for node types
+   // Enum for node types
    enum class Type {
         // Leaf nodes
         Literal,
@@ -44,7 +40,7 @@ struct Node {
         BasicType, // Covers types like INT, CHAR, etc.
         ArrayType,
         Type, // Represents type -> BOOLEAN | INT | ...
-        Poison,
+        Corrupted,
 
         // Program structure
         Program,
@@ -126,137 +122,74 @@ struct Node {
         LAST_MEMBER // Marker for bounds checking
     };
 
-
-   /// @brief String representations of the enum values
-   static constexpr std::array<std::string_view, static_cast<size_t>(Type::LAST_MEMBER)> type_strings{
-        "Literal", "Identifier", "QualifiedName", "Operator", "BasicType",
-        "ArrayType", "Type", "Poison", "Program", "PackageDeclaration",
-        "ImportDeclarations", "ImportDeclaration", "TypeDeclaration",
-        "ClassDeclaration", "InterfaceDeclaration", "ModifierList",
-        "PublicModifier", "ProtectedModifier", "StaticModifier",
-        "AbstractModifier", "FinalModifier", "ClassBody",
-        "ClassBodyDeclaration", "FieldDeclaration", "MethodDeclaration",
-        "ConstructorDeclaration", "SuperclassOpt", "InterfacesOpt",
-        "InterfaceList", "InterfaceBody", "InterfaceMemberDeclarationList",
-        "MethodHeader", "FormalParameterList", "FormalParameter",
-        "VoidType", "Statement", "Block", "IfStatement",
-        "IfElseStatement", "WhileStatement", "ForStatement",
-        "ReturnStatement", "EmptyStatement", "ExpressionStatement",
-        "VariableDeclarator", "LocalVariableDeclaration",
-        "VariableDeclaratorList", "FieldInitializer", "Expression",
-        "ArgumentList", "FieldAccess", "ArrayAccess", "CastExpression",
-        "MethodInvocation", "ArrayCreationExpression",
-        "ClassInstanceCreationExpression", "Dims", "NumLiteral",
-        "CharLiteral", "StringLiteral", "BooleanLiteral", "NullLiteral",
-        "TestExpression"
-    };
-
-
-   /// @brief Convert Type to string
-   static std::string_view to_string(Type type) {
-      size_t index = static_cast<size_t>(type);
-      if (index >= type_strings.size()) {
-         throw std::out_of_range("Invalid enum value for Type");
-      }
-      return type_strings[index];
-   }
-
-   /// @brief Protected constructor for leaf nodes
+   /// leaf nodes
    explicit Node(Type type) : type{type}, args{nullptr}, num_args{0} {}
 
-   /// @brief Protected constructor for non-leaf nodes
-   template <typename... Args>
-   Node(BumpPtrAllocator& alloc, Type type, Args&&... args)
-         : type{type},
-           args{static_cast<Node**>(alloc.allocate_bytes(
-                 sizeof...(Args) * sizeof(Node*), alignof(Node*)))} {
-      static_assert(sizeof...(Args) > 0, "Must have at least one child");
-      static_assert((std::is_convertible_v<Args, Node*> && ...),
-                    "All arguments must be convertible to Node*");
-      num_args = sizeof...(Args);
-      std::array<Node*, sizeof...(Args)> tmp{std::forward<Args>(args)...};
-      for (size_t i = 0; i < sizeof...(Args); ++i) {
-         this->args[i] = tmp[i];
-      }
-   }
+   // Non leaf nodes
+   template <typename... Args_>
+    Node(Type type, Args_&&... args_)
+        : type{type},
+          args{std::make_unique<Node*[]>(sizeof...(Args_))},
+          num_args{sizeof...(Args_)} {
+        static_assert(sizeof...(Args_) > 0, "Must have at least one child");
+        static_assert((std::is_convertible_v<Args_, Node*> && ...),
+                      "All arguments must be convertible to Node*");
 
-   /// @brief Gets the number of children
-   size_t num_children() const { return num_args; }
+        Node* temp_args[] = {std::forward<Args_>(args_)...};
+        for (std::size_t i = 0; i < num_args; ++i) {
+            args[i] = temp_args[i];
+        }
+    }
 
-   /// @brief Gets the child at index i
-   Node* child(size_t i) const { return args[i]; }
+   size_t get_num_children() const { return num_args; }
 
-   /// @brief Gets the type of the node
+   // Gets the child at index i of this child
+   Node* child_at(size_t i) const { return args[i]; }
+
+   // Gets node type
    Type get_node_type() const { return type; }
 
-   /// @brief Get string representation of the node's type
-   std::string type_string() const { return std::string(to_string(type)); }
-
-   /// @brief Check if the tree has been poisoned
-   bool is_poisoned() const {
-      if (type == Type::Poison) return true;
+   // If the node is corrupted, the tree has been corrupted
+   bool is_corrupted() const {
+      if (type == Type::Corrupted) return true;
       for (size_t i = 0; i < num_args; ++i) {
          if (args[i] == nullptr) continue;
-         if (args[i]->is_poisoned()) return true;
+         if (args[i]->is_corrupted()) return true;
       }
       return false;
    }
 
-   /// @brief Virtual function to print the node
    virtual std::ostream& print(std::ostream& os) const;
-
-   /// @brief Print the node as a dot file
-   std::ostream& printDot(std::ostream& os) const;
 
 private:
    Type type;
    Node** args;
    size_t num_args;
-
-   void printType(std::ostream& os) const;
-   void printTypeAndValue(std::ostream& os) const;
-   int printDotRecursive(std::ostream& os, const Node& node, int& id_counter) const;
 };
-
 
 // Output stream operator for a parse tree node
 std::ostream& operator<<(std::ostream& os, Node const& n);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief A lex node in the parse tree representing a literal value.
+// A node in the parse tree representing a literal value.
 class Literal : public Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
 public:
-   /// @brief Enum for literal types
+   // Enum for literal types
    enum class Type {
       Integer,
+      Boolean,
       Character,
       String,
-      Boolean,
-      Null,
-      LAST_MEMBER // Marker for bounds checking
+      Null
    };
-
-   /// @brief String representations of the literal types
-   static constexpr std::array<std::string_view, static_cast<size_t>(Type::LAST_MEMBER)> literal_strings{
-      "Integer", "Character", "String", "Boolean", "Null"
-   };
-
-   /// @brief Convert Type to string
-   static std::string_view to_string(Type type) {
-      size_t index = static_cast<size_t>(type);
-      if (index >= literal_strings.size()) {
-         throw std::out_of_range("Invalid enum value for Literal::Type");
-      }
-      return literal_strings[index];
-   }
 
 private:
-   /// @brief Constructor for Literal
-   Literal(BumpPtrAllocator const& alloc, Type type, char const* value)
+   // Constructor for Literal
+   Literal(Type type, char const* value)
          : Node{Node::Type::Literal},
            type{type},
            isNegative{false},
@@ -265,62 +198,53 @@ private:
 public:
    // Override printing for this leaf node
    std::ostream& print(std::ostream& os) const override {
-      os << "Literal(Type: " << to_string(type) << ", Value: " << value;
+      os << "Literal(Type: " << type << ", Value: " << value;
       if (isNegative) os << ", Negative: true";
       os << ")";
       return os;
    }
 
-   // Set the value of the literal to negative
    void setNegative() { isNegative = true; }
-
-   // Check if the literal is valid
-   bool isValid() const {
-      // Implement validation logic if needed
-      return !value.empty();
-   }
+   bool isValid() const;
 
 private:
    Type type;
    bool isNegative;
-   std::pmr::string value;
+   std::string value;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief A lex node in the parse tree representing an identifier.
+// A node representing an identifier.
 class Identifier : public Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
 private:
-   /// @brief Constructor for Identifier
-   Identifier(BumpPtrAllocator const& alloc, char const* name)
-         : Node{Node::Type::Identifier}, name{name, alloc} {}
+   Identifier(char const* name)
+         : Node{Node::Type::Identifier}, name{name} {}
 
 public:
-   // Get the name of the identifier
    const char* get_name() const { return name.c_str(); }
 
-   // Override printing for this leaf node
    std::ostream& print(std::ostream& os) const override {
       os << "Identifier(Name: " << name << ")";
       return os;
    }
 
 private:
-   std::pmr::string name;
+   std::string name;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief A lex node in the parse tree representing an operator.
+// A node representing  operator.
 class Operator : public Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
 public:
-   /// @brief Enum for operator types
+   // Enum for operator types
    enum class Type {
       Assign,
       GreaterThan,
@@ -334,8 +258,6 @@ public:
       Or,
       BitwiseAnd,
       BitwiseOr,
-      BitwiseXor,
-      BitwiseNot,
       Add,
       Subtract,
       Multiply,
@@ -343,41 +265,16 @@ public:
       Modulo,
       Plus,
       Minus,
-      InstanceOf,
-      LAST_MEMBER // Marker for bounds checking
+      InstanceOf
    };
-
-   /// @brief String representations of the operator types
-   static constexpr std::array<std::string_view, static_cast<size_t>(Type::LAST_MEMBER)> operator_strings{
-      "Assign", "GreaterThan", "LessThan", "Not", "Equal", 
-      "LessThanOrEqual", "GreaterThanOrEqual", "NotEqual", "And", "Or", 
-      "BitwiseAnd", "BitwiseOr", "BitwiseXor", "BitwiseNot", 
-      "Add", "Subtract", "Multiply", "Divide", "Modulo", 
-      "Plus", "Minus", "InstanceOf"
-   };
-
-   /// @brief Convert Type to string
-   static std::string_view to_string(Type type) {
-      size_t index = static_cast<size_t>(type);
-      if (index >= operator_strings.size()) {
-         throw std::out_of_range("Invalid enum value for Operator::Type");
-      }
-      return operator_strings[index];
-   }
 
 private:
-   /// @brief Constructor for Operator
+   // Constructor for Operator
    explicit Operator(Type type) : Node{Node::Type::Operator}, type{type} {}
 
 public:
-   // Override printing for this leaf node
    std::ostream& print(std::ostream& os) const override {
-      return os << to_string();
-   }
-
-   // Get the string representation of the operator
-   std::string to_string() const {
-      return std::string(to_string(type));
+      return os << type;
    }
 
 private:
@@ -386,98 +283,66 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief A lex node in the parse tree representing a modifier.
+// A node representing modifier.
 class Modifier : public Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
 public:
-   /// @brief Enum for modifier types
+   // Enum for modifier types
    enum class Type {
       Public,
       Protected,
       Static,
       Abstract,
-      Final,
-      Native,
-      LAST_MEMBER // Marker for bounds checking
+      Final
    };
-
-   /// @brief String representations of the modifier types
-   static constexpr std::array<std::string_view, static_cast<size_t>(Type::LAST_MEMBER)> modifier_strings{
-      "Public", "Protected", "Static", "Abstract", "Final", "Native"
-   };
-
-   /// @brief Convert Type to string
-   static std::string_view to_string(Type type) {
-      size_t index = static_cast<size_t>(type);
-      if (index >= modifier_strings.size()) {
-         throw std::out_of_range("Invalid enum value for Modifier::Type");
-      }
-      return modifier_strings[index];
-   }
 
 private:
-   /// @brief Constructor for Modifier
-   explicit Modifier(Type type) : Node{Node::Type::Modifier}, modty{type} {}
+   // Constructor for Modifier
+   explicit Modifier(Type type) : Node{Node::Type::Modifier}, type{type} {}
 
 public:
    // Get the type of the modifier
-   Type get_type() const { return modty; }
+   Type get_type() const { return type; }
 
    // Print the string representation of the modifier
    std::ostream& print(std::ostream& os) const override {
-      os << "Modifier(Type: " << to_string(modty) << ")";
+      os << "Modifier(Type: " << type << ")";
       return os;
    }
 
 private:
-   Type modty;
+   Type type;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// @brief A lex node in the parse tree representing a basic type.
+// A lex node in the parse tree representing a basic type.
 class BasicType : public Node {
    friend class ::FlexLexer;
    friend class ::BisonParser;
 
 public:
-   /// @brief Enum for basic types
+   // Enum for basic types
    enum class Type {
       Byte,
       Short,
       Int,
       Char,
-      Boolean,
-      LAST_MEMBER // Marker for bounds checking
+      Boolean
    };
-
-   /// @brief String representations of the basic types
-   static constexpr std::array<std::string_view, static_cast<size_t>(Type::LAST_MEMBER)> basic_type_strings{
-      "Byte", "Short", "Int", "Char", "Boolean"
-   };
-
-   /// @brief Convert Type to string
-   static std::string_view to_string(Type type) {
-      size_t index = static_cast<size_t>(type);
-      if (index >= basic_type_strings.size()) {
-         throw std::out_of_range("Invalid enum value for BasicType::Type");
-      }
-      return basic_type_strings[index];
-   }
 
 private:
-   /// @brief Constructor for BasicType
+   // Constructor for BasicType
    explicit BasicType(Type type) : Node{Node::Type::BasicType}, type{type} {}
 
 public:
-   // Get the type of the basic type
    Type get_type() const { return type; }
 
    // Print the string representation of the basic type
    std::ostream& print(std::ostream& os) const override {
-      os << "BasicType(Type: " << to_string(type) << ")";
+      os << "BasicType(Type: " << type << ")";
       return os;
    }
 
