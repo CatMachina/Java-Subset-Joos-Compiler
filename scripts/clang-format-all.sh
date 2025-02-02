b3fa7ae761de147d37eb61f90ae01cfb5a92844e
Angel Zhang <a259zhan@uwaterloo.ca>
2025-02-01 19:13:33 -0500

    Can specify a subdirectory in clang-format-all script

--- scripts/clang-format-all.sh
+++ scripts/clang-format-all.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/bash
 
-find $HOME/cs444/joosc -type f \( -name "*.hpp" -o -name "*.cpp" \) -exec clang-format -i {} \;
+if [ $# -eq 0 ]; then
+    DIR=$HOME/cs444/joosc
+elif [ $# -eq 1 ]; then
+    DIR=$1
+fi
+
+echo "Running clang-format in $DIR"
+find $DIR -type f \( -name "*.hpp" -o -name "*.cpp" \) -exec clang-format -i {} \;
