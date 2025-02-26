93a5d21b9fc2fade0663e902057442ffdb8fcee8
e32xu <e32xu@uwaterloo.ca>
2025-02-26 04:02:27 -0500

    more draft?

--- include/staticCheck/nameDisambiguator.hpp
+++ include/staticCheck/nameDisambiguator.hpp
@@ -6,18 +6,63 @@
 
 namespace static_check {
 
-class NameDisambiguator {
+class ExprName {
 public:
-  NameDisambiguator(std::unique_ptr<parsetree::ast::ASTManager> astManager,
-                    std::unique_ptr<EnvManager> envManager)
-      : astManager(std::move(astManager)), envManager(std::move(envManager)) {}
+  enum class Type {
+    PackageName,
+    TypeName,
+    ExpressionName,
+    MethodName,
+    SingleAmbiguousName
+  }
 
-  void disambiguate(
-      std::shared_ptr<parsetree::ast::QualifiedIdentifier> qualifiedIdentifier);
+  ExprName(Type type,
+           std::shared_ptr<parsetree::ast::MemberName> node) : type(type),
+      node(node) {}
+
+  void reclassify(Type type, std::shared_ptr<parsetree::ast::MemberName> node) {
+    this->type = type;
+    this->node = node;
+  }
+
+  // Getters
+  std::shared_ptr<parsetree::ast::MemberName> getNode() { return node; }
+  Type getType() { return type; }
 
 private:
+  Type type;
+  std::shared_ptr<parsetree::ast::MemberName> node;
+};
+
+class NameDisambiguator {
+public:
+  NameDisambiguator(std::unique_ptr<parsetree::ast::ASTManager> astManager)
+      : astManager(std::move(astManager)) {}
+
+  std::shared_ptr<ExprName>
+  disambiguate(std::shared_ptr<parsetree::ast::MemberName> node);
+
+  void resolve() {
+    for (auto ast : astManager->getASTs()) {
+      resolveRecursive(ast);
+    }
+  }
+
+private:
+  void resolveAST(std::shared_ptr<parsetree::ast::AstNode> node);
+
+  void beginProgram(std::shared_ptr<parsetree::ast::ProgramDecl> programDecl) {
+    currentProgramDecl = programDecl;
+  }
+  void beginContext((std::shared_ptr<parsetree::ast::CodeBody> ctx) {
+    currentContext = ctx; }
+
   std::unique_ptr<parsetree::ast::ASTManager> astManager;
-  std::unique_ptr<EnvManager> envManager;
+  // hard to reuse envManager? this is my bad of not implementing it nicely
+  // std::unique_ptr<EnvManager> envManager;
+
+  std::shared_ptr<parsetree::ast::ProgramDecl> currentProgramDecl;
+  std::shared_ptr<parsetree::ast::CodeBody> currentContext;
 };
 
 } // namespace static_check
--- src/staticCheck/nameDisambiguator.cpp
+++ src/staticCheck/nameDisambiguator.cpp
@@ -8,9 +8,118 @@ bool NameDisambiguator::isInstanceField(std::string &id) { return true; }
 // TODO: is this a static field in the current class?
 bool NameDisambiguator::isStaticField(std::string &id) { return true; }
 
+std::shared_ptr<parsetree::ast::Decl>
+lookupNamedDecl(std::shared_ptr<parsetree::ast::DeclContext> ctx,
+                std::string_view name) {
+  auto condition = [name, this](std::shared_ptr<parsetree::ast::Decl> decl) {
+    if (auto typedDecl =
+            std::dynamic_pointer_cast<parsetree::ast::TypedDecl>(decl);
+        typedDecl) {
+      bool scopeVisible =
+          !this->lscope_ || this->lscope_->canView(typedDecl->scope());
+      bool canAccess = true;
+      if (auto fieldDecl =
+              std::dynamic_pointer_cast<parsetree::ast::FieldDecl>(decl)) {
+        canAccess =
+            isAccessible(fieldDecl->getModifiers(), fieldDecl->getParent());
+      }
+      return decl->getName() == name && scopeVisible && canAccess;
+    }
+    return false;
+  };
+
+  if (auto classDecl =
+          std::dynamic_pointer_cast<parsetree::ast::ClassDecl>(ctx);
+      classDecl) {
+    std::shared_ptr<parsetree::ast::Decl> result = nullptr;
+    // how to get inherited members? in hierarchy checking?
+    for (const auto decl : getInheritedMembers(classDecl)) {
+      if (condition(decl)) {
+        if (result)
+          return nullptr; // Ambiguous case
+        result = decl;
+      }
+    }
+    return result;
+  }
+  for (const auto decl : ctx->getDecls()) {
+    if (condition(decl))
+      return decl;
+  }
+  return nullptr;
+}
+
+bool NameDisambiguator::reclassifyDecl(
+    std::shared_ptr<parsetree::ast::CodeBody> ctx,
+    std::shared_ptr<ExprName> data) {
+  if (auto decl = lookupNamedDecl(currentContext, data->getNode()->getName())) {
+    if (auto varDecl =
+            std::dynamic_pointer_cast<parsetree::ast::VarDecl>(decl)) {
+      data->reclassify(ExprName::Type::ExpressionName, varDecl);
+      return true;
+    } else if (auto fieldDecl =
+                   std::dynamic_pointer_cast<parsetree::ast::FieldDecl>(decl)) {
+      data->reclassify(ExprName::Type::ExpressionName, fieldDecl);
+      return true;
+    }
+  }
+  if (auto ctxDecl = std::dynamic_pointer_cast<parsetree::ast::Decl>(ctx)) {
+    if (auto parentCtx = std::dynamic_pointer_cast<parsetree::ast::CodeBody>(
+            ctxDecl->getParent())) {
+      return reclassifyDecl(parentCtx, data);
+    }
+  }
+  return false;
+}
+
 // JLS 6.5.2
-void NameDisambiguator::disambiguate(
-    std::shared_ptr<parsetree::ast::QualifiedIdentifier> qualifiedIdentifier) {
+std::shared_ptr<ExprName> NameDisambiguator::disambiguate(
+    std::shared_ptr<parsetree::ast::MemberName> node) {
+  /*
+  JLS 6.5.2:
+  1. local variable decl, param decl, or field decl → ExpressionName.
+  2. local or member types (eg. local class) → TypeName.
+  3. in top-level class/interface in the same compilation unit or
+  single-type-import declaration → TypeName.
+  4. types in the same package (other compilation units) → TypeName.
+  5. type-import-on-demand in same compilation unit: Exactly one match →
+  TypeName, else → Compile-time error
+  6. none of the above, assume it's a package name → PackageName.
+
+  May be we can split it into reclassify Decl and reclassify import?
+  Decl: Search in this context, if not found, search parent context
+  Import: search in type linker context
+  */
+  // before being a single ambiguous name, it could be method
+  if (auto method =
+          std::dynamic_pointer_cast<parsetree::ast::MethodName>(node)) {
+    return std::make_shared<ExprName>(ExprName::Type::MethodName, method);
+  }
+
+  std::shared_ptr<ExprName> data =
+      std::make_shared<ExprName>(ExprName::Type::SingleAmbiguousName, node);
+
+  if (reclassifyDecl(currentContext, data))
+    return data;
+
+  // imports
+  auto import =
+      typeLinker->getImport(currentContext, data->getNode()->getName());
+  if (!import) {
+    throw std::runtime_error("No import for " + data->getNode()->getName());
+  }
+  if (auto decl = std::get_if<std::shared_ptr<parsetree::ast::Decl>>(import)) {
+    if (!decl) {
+      throw std::runtime_error("Ambiguous import-on-demand conflict");
+    }
+    data->reclassify(ExprName::Type::TypeName, decl);
+    return data;
+  } else if (auto pkg = std::get_if<std::shared_ptr<Package>>(import)) {
+    data->reclassify(ExprName::Type::PackageName, pkg);
+    return data;
+  }
+
+  /*
   std::vector<std::string> &identifiers =
       qualifiedIdentifiers->getIdentifiers();
   // 1. If local variable a_1 is in scope, we use it
@@ -49,6 +158,7 @@ void NameDisambiguator::disambiguate(
       }
     }
   }
+  */
   throw std::runtime_error("Fail to disambiguate");
 }
 
@@ -56,21 +166,46 @@ void NameDisambiguator::resolveAST(
     std::shared_ptr<parsetree::ast::AstNode> node) {
   if (!node)
     throw std::runtime_error("Node is null when resolving AST");
-  for (auto child : node->getChilden()) {
-    if (!child)
-      continue;
-    // Case: Qualified Name
-    if (auto qualifiedIdentifier =
-            std::dynamic_pointer_cast<parse::ast::QualifiedIdentifier>(node)) {
-      disambiguate(qualifiedIdentifier);
-    }
-    // TODO: Method Invocation?
-  }
-}
+  if (auto cu =
+          std::dynamic_pointer_cast<parsetree::ast::CompilationUnit>(node))
+    this->beginProgram(cu);
+  if (auto ctx = std::dynamic_pointer_cast<parsetree::ast::DeclContext>(node))
+    this->beginContext(ctx);
+  if (auto classDecl =
+          std::dynamic_pointer_cast<parsetree::ast::ClassDecl>(node))
+    currentClass = classDecl;
 
-void NameDisambiguator::resolve() {
-  for (auto ast : astManager->getASTs()) {
-    resolveAST(ast->getBody());
+  if (auto field = std::dynamic_pointer_cast<parsetree::ast::FieldDecl>(node)) {
+    isStaticContext = field->getModifiers().isStatic();
+    if (field->hasInit()) {
+      isInstFieldInitializer = !field->getModifiers().isStatic();
+      fieldScope = field->init()->scope();
+    }
+  } else if (auto method =
+                 std::dynamic_pointer_cast<parsetree::ast::MethodDecl>(node)) {
+    isStaticContext = method->getModifiers().isStatic();
+  }
+
+  if (auto decl = std::dynamic_pointer_cast<parsetree::ast::TypedDecl>(node)) {
+    if (auto init = decl->getInit()) {
+      evaluate(init);
+    }
+  } else if (auto stmt =
+                 std::dynamic_pointer_cast<parsetree::ast::Stmt>(node)) {
+    for (auto expr : stmt->getExprs()) {
+      if (!expr)
+        continue;
+      evaluate(expr);
+    }
+  }
+
+  if (std::dynamic_pointer_cast<parsetree::ast::DeclStmt>(node))
+    return;
+
+  for (auto child : node->getChildren() | std::views::filter([](auto c) {
+                      return c != nullptr;
+                    })) {
+    resolveAST(child);
   }
 }
 
