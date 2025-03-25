390e25bb49aa777426d853b305a2746adba2e368
dy5zhang <dy5zhang@uwaterloo.ca>
2025-03-25 17:56:25 -0400

    Simulator: first draft done

--- include/tir/Simluator.hpp
+++ include/tir/Simluator.hpp
@@ -31,7 +31,11 @@ namespace tir {
  * Based on /u/cs444/pub/tir/src/ir/interpret/Configuration.java
  */
 struct Configuration {
-  /* Some special stack-related names that are used in the IR */
+  /** Some special stack-related names that are used in the IR,
+   *
+   * Made these into functions since vscode was throwing an error about string
+   * field initializers not being allowed
+   */
   /** Prefix for argument registers */
   static std::string ABSTRACT_ARG_PREFIX() { return "_ARG"; }
   static std::string ABSTRACT_RET() { return "_RET"; }
@@ -45,6 +49,7 @@ class Simulator {
   /** Forward Declarations */
 private:
   class ExecutionFrame;
+  class ExprStack;
 
 protected:
   /** map from address to instruction */
@@ -69,7 +74,7 @@ private:
   /** heap size maximum **/
   int heapSizeMax;
 
-  // ExprStack exprStack;
+  std::unique_ptr<ExprStack> exprStack;
   // BufferedReader inReader;
 
   std::unordered_set<std::string> libraryFunctions;
@@ -122,167 +127,144 @@ protected:
   }
 
   void leave(std::shared_ptr<ExecutionFrame> frame) {
-    // Node insn = frame.getCurrentInsn();
-    // if (insn instanceof Const)
-    //   exprStack.pushValue(((Const)insn).value());
-    // else if (insn instanceof Temp)
-    // {
-    //   String tempName = ((Temp)insn).name();
-    //   exprStack.pushTemp(frame.get(tempName), tempName);
-    // }
-    // else if (insn instanceof BinOp)
-    // {
-    //   int r = exprStack.popValue();
-    //   int l = exprStack.popValue();
-    //   int result;
-    //   switch (((BinOp)insn).opType())
-    //   {
-    //   case ADD:
-    //     result = l + r;
-    //     break;
-    //   case SUB:
-    //     result = l - r;
-    //     break;
-    //   case MUL:
-    //     result = l * r;
-    //     break;
-    //   case DIV:
-    //     if (r == 0)
-    //       throw new Trap("Division by zero!");
-    //     result = l / r;
-    //     break;
-    //   case MOD:
-    //     if (r == 0)
-    //       throw new Trap("Division by zero!");
-    //     result = l % r;
-    //     break;
-    //   case AND:
-    //     result = l & r;
-    //     break;
-    //   case OR:
-    //     result = l | r;
-    //     break;
-    //   case XOR:
-    //     result = l ^ r;
-    //     break;
-    //   case LSHIFT:
-    //     result = l << r;
-    //     break;
-    //   case RSHIFT:
-    //     result = l >>> r;
-    //     break;
-    //   case ARSHIFT:
-    //     result = l >> r;
-    //     break;
-    //   case EQ:
-    //     result = l == r ? 1 : 0;
-    //     break;
-    //   case NEQ:
-    //     result = l != r ? 1 : 0;
-    //     break;
-    //   case LT:
-    //     result = l < r ? 1 : 0;
-    //     break;
-    //   case GT:
-    //     result = l > r ? 1 : 0;
-    //     break;
-    //   case LEQ:
-    //     result = l <= r ? 1 : 0;
-    //     break;
-    //   case GEQ:
-    //     result = l >= r ? 1 : 0;
-    //     break;
-    //   default:
-    //     throw new InternalCompilerError("Invalid binary operation");
-    //   }
-    //   exprStack.pushValue(result);
-    // }
-    // else if (insn instanceof Mem)
-    // {
-    //   int addr = exprStack.popValue();
-    //   exprStack.pushAddr(read(addr), addr);
-    // }
-    // else if (insn instanceof Call)
-    // {
-    //   int argsCount = ((Call)insn).args().size();
-    //   int args[] = new int[argsCount];
-    //   for (int i = argsCount - 1; i >= 0; --i)
-    //     args[i] = exprStack.popValue();
-    //   StackItem target = exprStack.pop();
-    //   String targetName;
-    //   if (target.type == StackItem.Kind.NAME)
-    //     targetName = target.name;
-    //   else if (indexToInsn.containsKey(target.value))
-    //   {
-    //     Node node = indexToInsn.get(target.value);
-    //     if (node instanceof FuncDecl)
-    //       targetName = ((FuncDecl)node).name();
-    //     else
-    //       throw new InternalCompilerError("Call to a non-function
-    //       instruction!");
-    //   }
-    //   else
-    //     throw new InternalCompilerError("Invalid function call '" + insn + "'
-    //     (target '" + target.value + "' is unknown)!");
+    std::shared_ptr<Node> insn = frame->getCurrentInsn();
+    if (auto constNode = std::dynamic_pointer_cast<Const>(insn))
+      exprStack->pushValue(constNode->getValue());
+    else if (auto tempNode = std::dynamic_pointer_cast<Temp>(insn)) {
+      std::string tempName = tempNode->getName();
+      exprStack->pushTemp(frame->get(tempName), tempName);
+    } else if (auto binOpNode = std::dynamic_pointer_cast<BinOp>(insn)) {
+      int r = exprStack->popValue();
+      int l = exprStack->popValue();
+      int result;
+      switch (binOpNode->op) {
+      case BinOp::OpType::ADD:
+        result = l + r;
+        break;
+      case BinOp::OpType::SUB:
+        result = l - r;
+        break;
+      case BinOp::OpType::MUL:
+        result = l * r;
+        break;
+      case BinOp::OpType::DIV:
+        if (r == 0)
+          throw new std::runtime_error("Division by zero!");
+        result = l / r;
+        break;
+      case BinOp::OpType::MOD:
+        if (r == 0)
+          throw new std::runtime_error("Division by zero!");
+        result = l % r;
+        break;
+      case BinOp::OpType::AND:
+        result = l & r;
+        break;
+      case BinOp::OpType::OR:
+        result = l | r;
+        break;
+      /** TODO: These operations aren't handled yet */
+      // case BinOp::OpType::XOR:
+      //   result = l ^ r;
+      //   break;
+      // case BinOp::OpType::LSHIFT:
+      //   result = l << r;
+      //   break;
+      // case BinOp::OpType::RSHIFT:
+      //   result = l >>> r;
+      //   break;
+      // case BinOp::OpType::ARSHIFT:
+      //   result = l >> r;
+      //   break;
+      case BinOp::OpType::EQ:
+        result = l == r ? 1 : 0;
+        break;
+      case BinOp::OpType::NEQ:
+        result = l != r ? 1 : 0;
+        break;
+      case BinOp::OpType::LT:
+        result = l < r ? 1 : 0;
+        break;
+      case BinOp::OpType::GT:
+        result = l > r ? 1 : 0;
+        break;
+      case BinOp::OpType::LEQ:
+        result = l <= r ? 1 : 0;
+        break;
+      case BinOp::OpType::GEQ:
+        result = l >= r ? 1 : 0;
+        break;
+      default:
+        throw std::runtime_error("Invalid binary operation");
+      }
+      exprStack->pushValue(result);
+    } else if (auto memNode = std::dynamic_pointer_cast<Mem>(insn)) {
+      int addr = exprStack->popValue();
+      exprStack->pushAddr(read(addr), addr);
+    } else if (auto callNode = std::dynamic_pointer_cast<Call>(insn)) {
+      int argsCount = callNode->getNumArgs();
+      std::vector<int> args(argsCount, 0);
+      for (int i = argsCount - 1; i >= 0; --i)
+        args[i] = exprStack->popValue();
+      std::shared_ptr<StackItem> target = exprStack->pop();
+      std::string targetName;
+      if (target->type == StackItem::Kind::NAME)
+        targetName = target->name;
+      else if (indexToInsn.count(target->value)) {
+        std::shared_ptr<Node> node = indexToInsn[target->value];
+        if (auto funcDecl = std::dynamic_pointer_cast<FuncDecl>(node))
+          targetName = funcDecl->getName();
+        else
+          throw std::runtime_error("Call to a non-function instruction!");
+      } else
+        throw std::runtime_error("Invalid function call '" + insn->label() +
+                                 "'(target '" + std::to_string(target->value) +
+                                 "' is unknown)!");
 
-    //   int retVal = call(frame, targetName, args);
-    //   exprStack.pushValue(retVal);
-    // }
-    // else if (insn instanceof Name)
-    // {
-    //   String name = ((Name)insn).name();
-    //   exprStack.pushName(libraryFunctions.contains(name)
-    //                          ? -1
-    //                          : findLabel(name),
-    //                      name);
-    // }
-    // else if (insn instanceof Move)
-    // {
-    //   int r = exprStack.popValue();
-    //   StackItem stackItem = exprStack.pop();
-    //   switch (stackItem.type)
-    //   {
-    //   case MEM:
-    //     if (debugLevel > 0)
-    //       System.out.println("mem[" + stackItem.addr + "]=" + r);
-    //     store(stackItem.addr, r);
-    //     break;
-    //   case TEMP:
-    //     if (debugLevel > 0)
-    //       System.out.println("temp[" + stackItem.temp + "]=" + r);
-    //     frame.put(stackItem.temp, r);
-    //     break;
-    //   default:
-    //     throw new InternalCompilerError("Invalid MOVE!");
-    //   }
-    // }
-    // else if (insn instanceof Exp)
-    // {
-    //   // Discard result.
-    //   exprStack.pop();
-    // }
-    // else if (insn instanceof Jump)
-    //   frame.setIP(exprStack.popValue());
-    // else if (insn instanceof CJump)
-    // {
-    //   CJump irCJump = (CJump)insn;
-    //   int top = exprStack.popValue();
-    //   String label;
-    //   if (top == 0)
-    //     label = irCJump.falseLabel();
-    //   else if (top == 1)
-    //     label = irCJump.trueLabel();
-    //   else
-    //     throw new InternalCompilerError("Invalid value in CJUMP: expected
-    //     0/1, got " + top);
-    //   if (label != null)
-    //     frame.setIP(findLabel(label));
-    // }
-    // else if (insn instanceof Return)
-    // {
-    //   frame.ret = exprStack.popValue();
-    //   frame.setIP(-1);
-    // }
+      int retVal = call(frame, targetName, args);
+      exprStack->pushValue(retVal);
+    } else if (auto nameNode = std::dynamic_pointer_cast<Name>(insn)) {
+      std::string name = nameNode->getName();
+      exprStack->pushName(
+          libraryFunctions.contains(name) ? -1 : findLabel(name), name);
+    } else if (auto moveNode = std::dynamic_pointer_cast<Move>(insn)) {
+      int r = exprStack->popValue();
+      std::shared_ptr<StackItem> stackItem = exprStack->pop();
+      switch (stackItem->type) {
+      case StackItem::MEM:
+        if (debugLevel > 0)
+          std::cout << "mem[" << stackItem->addr << "]=" << r << std::endl;
+        store(stackItem->addr, r);
+        break;
+      case StackItem::TEMP:
+        if (debugLevel > 0)
+          std::cout << "temp[" << stackItem->temp << "]=" << r << std::endl;
+        frame->put(stackItem->temp, r);
+        break;
+      default:
+        throw std::runtime_error("Invalid MOVE!");
+      }
+    } else if (auto expNode = std::dynamic_pointer_cast<Exp>(insn)) {
+      // Discard result.
+      exprStack->pop();
+    } else if (auto jumpNode = std::dynamic_pointer_cast<Jump>(insn))
+      frame->setIP(exprStack->popValue());
+    else if (auto irCJump = std::dynamic_pointer_cast<CJump>(insn)) {
+      int top = exprStack->popValue();
+      std::string label;
+      if (top == 0)
+        label = irCJump->getFalseLabel();
+      else if (top == 1)
+        label = irCJump->getTrueLabel();
+      else
+        throw std::invalid_argument(
+            "Invalid value in CJUMP: expected 0/1, got " + top);
+      if (!label.empty())
+        frame->setIP(findLabel(label));
+    } else if (auto returnNode = std::dynamic_pointer_cast<Return>(insn)) {
+      frame->ret = exprStack->popValue();
+      frame->setIP(-1);
+    }
   }
 
 public:
@@ -310,8 +292,8 @@ public:
 
     mem = std::vector<int>();
 
-    // TODO: add back
-    // exprStack = new ExprStack();
+    exprStack = std::make_unique<ExprStack>();
+    // Don't think we need this
     // inReader = new BufferedReader(new InputStreamReader(System.in));
 
     // TODO: check this
@@ -321,7 +303,7 @@ public:
     libraryFunctions.insert("__exception");
     libraryFunctions.insert("NATIVEjava.io.OutputStream.nativeWrite");
 
-    // TODO: check if needed?
+    // TODO: probably needed
     // InsnMapsBuilder imb = new InsnMapsBuilder();
     // compUnit = (CompUnit)imb.visit(compUnit);
     // indexToInsn = imb.indexToInsn();
@@ -497,7 +479,7 @@ private:
      * this is postorder traversal, one step at a time, modulo jumps.
      */
     bool advance() {
-      // TODO: Wut
+      // TODO: Wut, only add if needed
       // Time out if necessary.
       // if (Thread.currentThread().isInterrupted())
       //   return false;
